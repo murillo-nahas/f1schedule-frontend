74a51ad698f8747b9bbe89d0b0bb16c70de406f2
Murillo Nahás <murillonahas2003@gmail.com>
2023-12-07 11:30:43 -0300

    feat: using map to render routes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,59 +4,49 @@ import Logo from "../../assets/images/Logo.png";
 import { IoIosSearch } from "react-icons/io";
 import { IoIosArrowDown } from "react-icons/io";
 
+interface HeaderRoute {
+  route: string;
+  label: string;
+}
+
+const headerRoutes: HeaderRoute[] = [
+  {
+    route: "/schedules",
+    label: "Schedules",
+  },
+  {
+    route: "/",
+    label: "Standings",
+  },
+  {
+    route: "/",
+    label: "Teams",
+  },
+  {
+    route: "/",
+    label: "Drivers",
+  },
+];
+
 export function Header() {
   return (
     <nav className="flex flex-row justify-between items-center p-8">
       <div className="flex flex-row justify-center items-center">
         <img src={Logo} alt="logo" />
         <ul className="flex flex-row list-none">
-          <div className="group">
-            <li className="ml-12 flex flex-row justify-center items-center">
-              <Link
-                to="/schedules"
-                className="text-black group-hover:text-red-600 transition-colors"
-              >
-                Schedules
-              </Link>
-              <IoIosArrowDown className="group-hover:text-red-600 ml-1" />
-            </li>
-          </div>
-
-          <div className="group">
-            <li className="ml-12 flex flex-row justify-center items-center">
-              <Link
-                to="/schedules"
-                className="text-black group-hover:text-red-600 transition-colors"
-              >
-                Standings
-              </Link>
-              <IoIosArrowDown className="group-hover:text-red-600 ml-1" />
-            </li>
-          </div>
-
-          <div className="group">
-            <li className="ml-12 flex flex-row justify-center items-center">
-              <Link
-                to="/schedules"
-                className="text-black group-hover:text-red-600 transition-colors"
-              >
-                Drivers
-              </Link>
-              <IoIosArrowDown className="group-hover:text-red-600 ml-1" />
-            </li>
-          </div>
-
-          <div className="group">
-            <li className="ml-12 flex flex-row justify-center items-center">
-              <Link
-                to="/schedules"
-                className="text-black group-hover:text-red-600 transition-colors"
-              >
-                Teams
-              </Link>
-              <IoIosArrowDown className="group-hover:text-red-600 ml-1" />
-            </li>
-          </div>
+          {headerRoutes.map((el) => (
+            <div className="group">
+              <li className="ml-12 flex flex-row justify-center items-center">
+                <Link
+                  to={el.route}
+                  className="text-black group-hover:text-red-600 transition-colors"
+                >
+                  {el.label}
+                </Link>
+                <IoIosArrowDown className="group-hover:text-red-600 ml-1" />
+              </li>
+            </div>
+          ))}
         </ul>
       </div>
 
