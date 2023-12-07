bf5f7388dce58f8e9e41136c65b1e88fbc66a15f
Murillo Nahás <murillonahas2003@gmail.com>
2023-12-07 15:50:10 -0300

    feat: grandprix GET request

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,45 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header/Header";
 
+interface GrandPrix {
+  id: number;
+  name: string;
+  description: string;
+  image: string;
+  start_date: Date;
+  end_date: Date;
+}
+
 export function Home() {
+  const [grandPrix, setGrandPrix] = useState<GrandPrix[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/grandprix");
+
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status}`);
+        }
+
+        const data = await response.json();
+        setGrandPrix(data);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <Header />
+      {grandPrix.map((item) => (
+        <div key={item.id}>
+          <p>{item.name}</p>
+        </div>
+      ))}
     </>
   );
 }
